c6e518de4d1903b46af4dca011610bf65b41f55a
BurakBenli <bbenli999@gmail.com>
2019-06-18 00:07:58 +0300

    ConvertXmlToJson Example

---
+++ src/main/java/ConvertXmlToJson.java
@@ -0,0 +1,19 @@
+import org.json.JSONObject;
+import org.json.XML;
+
+public class ConvertXmlToJson {
+
+    private static final int INDECT_FACTOR = 4;
+
+    public static void main(String[] args) {
+
+        String xmlString = "<note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>";
+
+        JSONObject jsonObject = XML.toJSONObject(xmlString);
+
+        String jsonResultString = jsonObject.toString(INDECT_FACTOR);
+
+        System.out.println(jsonResultString);
+
+    }
+}
